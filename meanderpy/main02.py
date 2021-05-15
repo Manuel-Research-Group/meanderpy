a6e5b33406703dfe1f94f9347ee4673d8497e178
Beuren Bechlin <beuren.bechlin@gmail.com>
2021-05-15 03:26:50 -0300

    test case update

--- meanderpy/main02.py
+++ meanderpy/main02.py
@@ -8,21 +8,14 @@ L = 20000
 ds = 100
 
 x = np.linspace(0, L, int(L/ds) + 1)
-y = 500 * np.exp(( 1.0 / L) * x) * np.cos(((x + 1500) / L) * 16 * np.pi) / (np.exp((x - 0.75 * L) / (0.025 * L)) + 1)
+y = 500 * np.exp(( 1.0 / L) * x) * np.cos(((x) / L) * 16 * np.pi) / (np.exp((x - 0.75 * L) / (0.025 * L)) + 1)
 
 z = np.tan(5.0 * np.pi / 180) / (2 * L) * (x ** 2 + L * ( L - 2 * x ) )
 
 ch_depth = lambda slope: 6 * slope ** 2
-dep_height = lambda slope: 4 * slope ** 2 
-
-def dep_props(slope): 
-    f = np.abs(slope) / np.max(np.abs(slope))
-    return (0.2 * f ** 2, 0.4, 0.5 * (1 - f) ** 2)
-
-def aggr_props(slope):
-    f = np.abs(slope) / np.max(np.abs(slope))
-    return (0.2 * f ** 2, 0.2, 0.4 * (1 - f) ** 2,)
-
+dep_height = lambda slope: 4 * slope ** 2
+dep_props = lambda slope: (0.1, 0.8, 0.1)
+aggr_props = lambda slope: (0.1, 0.8, 0.1)
 events = [
     mp.ChannelEvent(nit = 100, saved_ts = 25, mode='INCISION', ch_depth = ch_depth, dep_height = dep_height, dep_props = dep_props, aggr_props = aggr_props, kv = 0.0033 / ONE_YEAR),
     mp.ChannelEvent(nit = 100, saved_ts = 25, mode='AGGREGATION', ch_depth = ch_depth, dep_height = dep_height, dep_props = dep_props, aggr_props = aggr_props, aggr_factor=2, kv = 0.002 / ONE_YEAR),
